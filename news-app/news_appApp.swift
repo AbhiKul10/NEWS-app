fd982dcd2c8b4598db13e8b1c7bff9802d0ab14e
Abhi Kulshrestha <abhikulshrestha1@gmail.com>
2021-01-31 21:47:36 +0530

    Initial Commit

---
+++ news-app/news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_appApp.swift
+//  news-app
+//
+//  Created by Abhi Kulshrestha on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
